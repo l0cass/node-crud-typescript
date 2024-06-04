c29d4585a69d4149e1929fb5817c5ebc3f9272b4
Lucas Piffer <91209474+l0cass@users.noreply.github.com>
2024-06-04 19:36:49 +0000

    initial commit with typescript setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (_req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log(`listening on port ${port}.`))
